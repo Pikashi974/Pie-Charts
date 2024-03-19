738feaf84ee952978cc8cf8b2cac2af9a02a95c4
Jérémy PAYET <the_big_boss97437@hotmail.fr>
2024-03-19 08:30:03 +0400

    Change to only SVG download and link input

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,8 @@ const partsHolder = document.querySelector("#partsHolder");
 const generatePie = document.querySelector("#generatePie");
 const downloadPie = document.querySelector("#downloadPie");
 let nbParts = 0;
+let chart;
+let images = [];
 let options;
 function init() {
   changeNbParts();
@@ -25,20 +27,12 @@ function changeNbParts() {
     <label for="labelVal${index}">Label ${parseInt(index) + 1}</label>
 <input type="text" id="labelVal${index}" name="labelVal${index}" />
     </div>
-  <div class="form-check form-switch">
-    <input class="form-check-input" type="checkbox" value="" id="checkBox${index}" onchange="toggleInput(${index})">
-    <label class="form-check-label" for="checkBox${index}">Link</label>
-  </div>
   <div class="form-group" id="link${index}">
     <label for="formLink${index}" class="col-sm-2 col-form-label">Paste link here</label>
     <div class="col-sm-10">
       <input type="text" class="form-control" id="formLink${index}" value="">
     </div>
   </div>
-  <div class="form-group d-none" id="file${index}">
-      <label for="formFile${index}" class="form-label mt-4">Choose your image</label>
-      <input class="form-control" type="file" id="formFile${index}">
-    </div>
     <div class="form-group" id="quantity${index}">
     <label for="quantityVal${index}">Quantity</label>
 
@@ -58,67 +52,49 @@ function changeNbParts() {
   nbParts = partsValue;
 }
 
-function toggleInput(index) {
-  if (
-    document.querySelector(`label[for="checkBox${index}"]`).innerHTML == "Link"
-  ) {
-    document.querySelector(`label[for="checkBox${index}"]`).innerHTML = "File";
-  } else {
-    document.querySelector(`label[for="checkBox${index}"]`).innerHTML = "Link";
-  }
-  document.querySelector(`#link${index}`).classList.toggle("d-none");
-  document.querySelector(`#file${index}`).classList.toggle("d-none");
-}
-
 function cookPie() {
   let seriesTab = [];
-  let images = [];
+  images = [];
   let labelsVal = [];
   for (let index = 0; index < nbParts; index++) {
     const element = parseInt(
       document.querySelector(`#quantityVal${index}`).value
     );
     seriesTab.push(element);
-    if (document.querySelector(`#checkBox${index}`).checked) {
-      images.push(
-        URL.createObjectURL(
-          document.querySelector(`#formFile${index}`).files[0]
-        )
-      );
-    } else {
-      images.push(document.querySelector(`#formLink${index}`).value);
-    }
+    images.push(document.querySelector(`#formLink${index}`).value);
+
     labelsVal.push(
       document.querySelector(`#labelVal${index}`).value != ""
         ? document.querySelector(`#labelVal${index}`).value
         : `series${index}`
     );
   }
-  console.log(seriesTab);
+  // console.log(seriesTab);
   document.querySelector("#chart").innerHTML = "";
   options = {
     series: seriesTab,
     labels: labelsVal,
     chart: {
+      id: "LineGraph1",
       width: 380,
       type: "pie",
-      //   toolbar: {
-      //     show: true,
-      //     offsetX: 0,
-      //     offsetY: 0,
-      //     tools: {
-      //       download: true,
-      //       selection: true,
-      //       zoom: true,
-      //       zoomin: true,
-      //       zoomout: true,
-      //       pan: true,
-      //       reset: true | '<img src="/static/icons/reset.png" width="20">',
-      //       customIcons: [],
-      //     },
+      // toolbar: {
+      //   show: true,
+      //   offsetX: 0,
+      //   offsetY: 0,
+      //   tools: {
+      //     download: true,
+      //     selection: true,
+      //     zoom: true,
+      //     zoomin: true,
+      //     zoomout: true,
+      //     pan: true,
+      //     reset: true | '<img src="/static/icons/reset.png" width="20">',
+      //     customIcons: [],
       //   },
+      // },
     },
-    colors: ["#93C3EE", "#E5C6A0", "#669DB5", "#94A74A"],
+    // colors: ["#93C3EE", "#E5C6A0", "#669DB5", "#94A74A"],
     fill: {
       type: "image",
       opacity: 0.85,
@@ -156,17 +132,32 @@ function cookPie() {
       },
     ],
   };
-
-  var chart = new ApexCharts(document.querySelector("#chart"), options);
+  if (chart != undefined) {
+    chart.destroy();
+  }
+  chart = new ApexCharts(document.querySelector("#chart"), options);
   chart.render();
   downloadPie.removeAttribute("disabled");
 }
 
+function previewFile(file, position) {
+  const fileObject = file.files[0];
+  const reader = new FileReader();
+
+  reader.addEventListener(
+    "load",
+    () => {
+      // convert image file to base64 string
+      images[position] = reader.result;
+    },
+    false
+  );
+  if (file) {
+    reader.readAsDataURL(fileObject);
+  }
+}
+
 async function downloadSVG() {
-  domtoimage.toPng(document.querySelector("#chart")).then(function (blob) {
-    var link = document.createElement("a");
-    link.download = "Tierlist.png";
-    link.href = blob;
-    link.click();
-  });
+  const ctx = chart.ctx;
+  ctx.exports.exportToSVG(this.ctx);
 }
